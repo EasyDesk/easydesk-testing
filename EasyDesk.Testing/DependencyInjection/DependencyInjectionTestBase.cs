b016b88a2c4451dfe9fd9743bc6dbc69c7a2599f
Francesco Dente <dente.francesco1996@gmail.com>
2021-08-02 18:58:51 +0200

    Add base class for dependency-injection-reliant tests

---
+++ EasyDesk.Testing/DependencyInjection/DependencyInjectionTestBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EasyDesk.Testing.DependencyInjection
+{
+    public abstract class DependencyInjectionTestBase
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DependencyInjectionTestBase()
+        {
+            var container = new ServiceCollection();
+            ConfigureServices(container);
+            _serviceProvider = container.BuildServiceProvider();
+        }
+
+        protected abstract void ConfigureServices(IServiceCollection services);
+
+        protected T Service<T>() => _serviceProvider.GetRequiredService<T>();
+    }
+}
